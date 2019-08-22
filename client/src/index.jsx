18304a4d4f56c48dcf76a3a3ab19fa8414bd774d
Angelia Campbell <ali.belclamp@gmail.com>
2019-08-22 18:34:19 -0400

    add API to fetch movie from database based on :id parameter, which can match the title or _id of a document

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ItemData from './components/ItemData.jsx';
 
-ReactDOM.render(<ItemData />, document.getElementById('itemdata'));
+ReactDOM.render(<ItemData movieID={'5d5dd75e50795405cdc6fcb9'}/>, document.getElementById('itemdata'));
