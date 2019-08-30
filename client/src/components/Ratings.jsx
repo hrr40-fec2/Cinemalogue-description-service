d4bc8b1cc07df0ea7cf23d15f4e590cbc532aee1
Angelia Campbell <ali.belclamp@gmail.com>
2019-08-29 21:31:36 -0400

    stars field now hides until rate this button is clicked

--- client/src/components/Ratings.jsx
+++ client/src/components/Ratings.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import RateThisButton from './RateThisButton.jsx';
+import StarIcon from './StarIcon.jsx';
 
 class Ratings extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      displayForm: false
+    };
     this.handleInput = this.handleInput.bind(this);
+    this.handleRateThisClick = this.handleRateThisClick.bind(this);
   }
 
   handleInput (e) {
@@ -16,26 +22,46 @@ class Ratings extends React.Component {
       imdbRatings: newAmount,
       imdbRatingsAverage: Number(newAverage)
     }
+    this.setState({displayForm: false});
     //call method from parent that posts the update, gets new data and rerenders
     this.props.handleRatingInput(data);
+  }
+
+  handleRateThisClick () {
+    this.setState({displayForm: true});
+  }
+
+  renderRatingForm () {
+    if (this.state.displayForm) {
+      return (
+        <div className="stars">
+          <form>
+            <input type="radio" name="stars" value="1" onClick={(e) => {this.handleInput(e)}}></input>
+            <input type="radio" name="stars" value="2" onClick={(e) => {this.handleInput(e)}}></input>
+            <input type="radio" name="stars" value="3" onClick={(e) => {this.handleInput(e)}}></input>
+            <input type="radio" name="stars" value="4" onClick={(e) => {this.handleInput(e)}}></input>
+            <input type="radio" name="stars" value="5" onClick={(e) => {this.handleInput(e)}}></input>
+            <input type="radio" name="stars" value="6" onClick={(e) => {this.handleInput(e)}}></input>
+            <input type="radio" name="stars" value="7" onClick={(e) => {this.handleInput(e)}}></input>
+            <input type="radio" name="stars" value="8" onClick={(e) => {this.handleInput(e)}}></input>
+            <input type="radio" name="stars" value="9" onClick={(e) => {this.handleInput(e)}}></input>
+            <input type="radio" name="stars" value="10" onClick={(e) => {this.handleInput(e)}}></input>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <div className="stars"></div>
+      );
+    }
   }
 
   render () {
     return (
       <div className="imdbRatings">
         {this.props.average} stars, {this.props.amount} ratings
-        <form className="starrating">
-          <input type="radio" name="stars" value="1" onClick={(e) => {this.handleInput(e)}}></input>
-          <input type="radio" name="stars" value="2" onClick={(e) => {this.handleInput(e)}}></input>
-          <input type="radio" name="stars" value="3" onClick={(e) => {this.handleInput(e)}}></input>
-          <input type="radio" name="stars" value="4" onClick={(e) => {this.handleInput(e)}}></input>
-          <input type="radio" name="stars" value="5" onClick={(e) => {this.handleInput(e)}}></input>
-          <input type="radio" name="stars" value="6" onClick={(e) => {this.handleInput(e)}}></input>
-          <input type="radio" name="stars" value="7" onClick={(e) => {this.handleInput(e)}}></input>
-          <input type="radio" name="stars" value="8" onClick={(e) => {this.handleInput(e)}}></input>
-          <input type="radio" name="stars" value="9" onClick={(e) => {this.handleInput(e)}}></input>
-          <input type="radio" name="stars" value="10" onClick={(e) => {this.handleInput(e)}}></input>
-        </form>
+        <RateThisButton onClick={this.handleRateThisClick} ><StarIcon />Rate This</RateThisButton>
+        {this.renderRatingForm()}
       </div>
       );
 
