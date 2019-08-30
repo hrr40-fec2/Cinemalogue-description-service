8d0182ea2d5cf7c4c0de767b2a81aa08078fb24f
Angelia Campbell <ali.belclamp@gmail.com>
2019-08-29 21:32:04 -0400

    add new styled components for rate this button

---
+++ client/src/components/RateThisButton.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const RateThisButton = styled.button`
+  color: white;
+  background-color: none;
+`;
+
+export default RateThisButton;
---
+++ client/src/components/StarIcon.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import {StarEmpty} from 'styled-icons/icomoon/StarEmpty';
+
+const StarIcon = styled(StarEmpty)`
+  color: grey;
+  width: 30px;
+  height: 30px;
+`;
+
+export default StarIcon;
